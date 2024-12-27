c8dfc70465c34a697e858565b11f637201db624a
azoonex <amirjabic@gmail.com>
2024-12-26 23:07:31 -0800

    add new three

--- hello cube/src/main.js
+++ hello cube/src/main.js
@@ -1,24 +1,28 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const scene = new THREE.Scene();
+const geometry = new THREE.BoxGeometry(1,1,1);
+const material = new THREE.MeshBasicMaterial({color : 0x00ff00})
+const mesh = new THREE.Mesh(geometry,material);
+mesh.position.set(1,1,1)
+mesh.scale.set(3,1,1)
+mesh.rotation.set(1,5,1)
+scene.add(mesh)
 
-setupCounter(document.querySelector('#counter'))
+// camera
+const aspect = {
+    width : window.innerWidth,
+    height : window.innerHeight
+}
+const camera = new THREE.PerspectiveCamera(75,aspect.width / aspect.height,);
+// 3d => x / y / z => x -- y || z deep
+camera.position.set(1,1.70,4)
+scene.add(camera);
+
+
+const canvas = document.getElementById('canvas');
+const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize(aspect.width, aspect.height);
+renderer.render(scene, camera);
+
+// =========================
