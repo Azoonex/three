2fd91dc6b05a67e1b52deded8f9d0ce8328ac5f0
azoonex <amirjabic@gmail.com>
2025-01-03 11:20:13 -0800

    create the new boxgemoetry

--- hello cube/src/main.js
+++ hello cube/src/main.js
@@ -1,28 +1,56 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const geometry = new THREE.BoxGeometry(1,1,1);
+const group = new THREE.Group();
+
+const geometry = new THREE.BoxGeometry(2,2,2);
 const material = new THREE.MeshBasicMaterial({color : 0x00ff00})
 const mesh = new THREE.Mesh(geometry,material);
-mesh.position.set(1,1,1)
-mesh.scale.set(3,1,1)
-mesh.rotation.set(1,5,1)
-scene.add(mesh)
+mesh.position.y = 1;
+
+const geometry2 = new THREE.BoxGeometry(1,1,1);
+const material2 = new THREE.MeshBasicMaterial({color : "white"});
+const mesh2 = new THREE.Mesh(geometry2,material2);
+
+const Box3 = new THREE.BoxGeometry(1,1,1);
+const material3 = new THREE.MeshBasicMaterial({color : "green"});
+const mesh3 = new THREE.Mesh(geometry2,material2);
+
+mesh2.position.y = 3;
+mesh3.position.y = -2;
+
+group.add(mesh,mesh2,mesh3)
+group.position.x = 1
+scene.add(group)
+
+const axesHelper = new THREE.AxesHelper(6);
+scene.add(axesHelper);
 
 // camera
 const aspect = {
     width : window.innerWidth,
     height : window.innerHeight
-}
-const camera = new THREE.PerspectiveCamera(75,aspect.width / aspect.height,);
+};
+const camera = new THREE.PerspectiveCamera(100,aspect.width / aspect.height,);
 // 3d => x / y / z => x -- y || z deep
-camera.position.set(1,1.70,4)
+camera.position.set(1,1,4);
 scene.add(camera);
 
 
 const canvas = document.getElementById('canvas');
 const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize(aspect.width, aspect.height);
-renderer.render(scene, camera);
 
+
+const clock = new THREE.Clock();
+function animate (){
+    const elapsedTime = clock.getElapsedTime();
+    mesh.position.x = Math.sin(elapsedTime);
+    mesh.position.y = Math.acos(elapsedTime);
+
+
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(animate);
+}
+animate()
 // =========================
